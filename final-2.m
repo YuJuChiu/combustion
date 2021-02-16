486719a92f5263c6645c31793620540e0072736b
YuJuChiu <73531190+YuJuChiu@users.noreply.github.com>
2021-02-16 21:45:09 +0100

    translate the comment from mandarin to english

--- final-2.m
+++ final-2.m
@@ -1,19 +1,19 @@
 clear all
 clc
-%   step1  ³]©wªì©l±ø¥ó
+% step1  initialize the conditions
 T = 2400;
 Rbar = 8.314;
 
-% a¯x°} ¦U¤¸¯À¦b¤£¦Pª«½è¤¤¤§²öº¸¼Æ
-% ª«½è­Ì O2, CO, CO2, H2O, H, H2, OH, N2, NO
+% matrix a, mole of each substance 
+% O2, CO, CO2, H2O, H, H2, OH, N2, NO
 a = [0, 1, 1, 0, 0, 0, 0, 0, 0;
      2, 1, 2, 1, 0, 0, 1, 0, 1;
      0, 0, 0, 2, 1, 2, 1, 0, 0;
      0, 0, 0, 0, 0, 0, 0, 2, 1];
- % b¯x°} ¦U¤¸¯À¤§²öº¸¼Æ
+ % matrix b, mole of each element 
  b = [1, 3.8, 4, 14.288];
  
- % ¦Uª«½è¤§°ò¥»©Ê½è ¨ä¤¤dh=h2400-h298
+ % properties of each substance, dh=h2400-h298
 h0 = [0, -110541, -393546, -241845, 217977, 0, 38985, 0, 90297];
 dh = [74467, 71354, 115798, 93744, 43689, 66925, 67843, 70645, 72617];
 s0 = [275.627, 265.278, 320.333, 274.225, 157.956, 194.692, 248.654, 258.598, 279.722];  
@@ -23,28 +23,27 @@ for j = 1:9
     gstart(j) = h0(j) + dh(j) - T * s0(j);
 end
 
-% ¥ý²q´ú¥­¿Å«á¦Uª«½è¤§²öº¸¼Æ
+% guess the mole of each substance after balancing
 lnn = rand(1,9);
 lnN = rand(1);
 
 
-%   step2  ³]©w»Ý­n¥Îªº¯x°}¤Î¤èµ{¦¡­Ì¡A¨Ã¶}©l­¡¥N
+% step2  initialize the neccessary martices, start to iterate
 for times = 1:100
     g = zeros(1,9);
-    % ¢Þ¬°5*5¯x°}(4¤¸¯À+1)
+    % ï¼° = R^(5*5), (4 elements + 1)
     P = zeros(5);
     R = zeros(5,1);
-    % ±N²q´ú¤§¥­¿Å²öº¸¼Æ¨úln
+    % take logarithm of the mole
     n1 = exp(lnn);
     N = exp(lnN);
-    % gibbs eq ¨ä¤¤P=10atm¡APo=1atm¬G¬Ù²¤
+    % gibbs eq 
     for j = 1:9
         g(j) = gstart(j) + Rbar * T * log(10*n1(j)/N);
     end
     
     
-    %   step3   ³]©wP¡BR¯x°}¤§«Y¼Æ
-    % P¯x°}±qn,m=1,1¨ì4,4¥ý³]©w
+    % step3   initialize P, R matrix 
     for n = 1:4
         for m = 1:4
             for j = 1:9
@@ -52,43 +51,42 @@ for times = 1:100
             end
         end
     end
-	% P¯x°}³]©wn=5¤Îm=5¡A°£¤F³Ì¥k¤U¨¤¤§n,m=5,5
+    
     for i = 1:4  
         for j = 1:9
             P(i,5) = P(i,5) + n1(j) * a(i,j);
         end
         P(5,i) = P(i,5);
     end
-	% P¯x°}³]©wn,m=5,5
+    
     for j = 1:9   
         P(5,5) = P(5,5) + n1(j);
     end
     P(5,5) = P(5,5) - N;
 
-    % R¯x°}«e¥|¶µ
+    % R
     for i = 1:4
         for j = 1:9
             R(i) = R(i) + n1(j) * a(i,j) * (g(j)/Rbar/T - 1);
         end
         R(i) = R(i) + b(i);
     end
-    % R¯x°}³Ì«á¤@¶µ
+
     for j = 1:9
         R(5) = R(5) + n1(j) * (g(j)/Rbar/T - 1);
     end
     R(5) = R(5) + N;
 
   
-	%   step4   ±NP¨ú¤Ï¯x°}¡A¨Ã¦P®É«e­¼©óPQ=R¡Aºâ¥XQ¯x°}
+    % step4 
     Q = P\R ;
 
     
-    %   step5   ¨D­Óª«½è¤§²öº¸¼ÆÅÜ¤Æ¶q
-    % 9­Óª«½èªºÅÜ¤Æ¶qdlnn
+    % step5   the difference of mole of each substance
     dlnn = zeros(1,9);
     for j = 1:9
         for i = 1:4
-            % Q(i)=pi(i) (¦@4­Ó¤¸¯À)
+            % Q(i)=pi(i)
             dlnn(j) = dlnn(j) + a(i,j) * Q(i);
         end
         % Q(5)=dlnN
@@ -96,15 +94,12 @@ for times = 1:100
     end
 
     
-	%   step6   ¨ú¨ç¼Æ­¡¥N¤§¦¬ÀÄ­È
-    %¡@¨ú³Ì¤j»~®t­È
-    % ¨ä¤¤exp(lnn + dlnn)¬°ª«½è·í«e²öº¸¼Æ¡An1¬°ªì©lª«½è²öº¸¼Æ 
-    % ¥ý³B²z1-9­Óª«½è¤§n
+    % step6   take the value of convergence
     Emax = max(abs((exp(lnn + dlnn)-n1)/n1));
-    %¦A³B²zÁ`²öº¸¼ÆÅÜ¤Æ¶q¡A©ón1-9¤ÎN¤¤¨ú¤@­Ó³Ì¤jªº
+
     Emax = max(Emax,abs((exp(lnN + Q(5))-N)/N));
     
-    %   ¨ç¼Æ¦¬ÀÄ¡A¢ë¢ð¢í¢ë¢ó¦U¤¸¯À¤§²öº¸¼Æ
+    % converge
     if Emax < 1E-10
         element = zeros(1,4);
         for i = 1:4
@@ -113,21 +108,21 @@ for times = 1:100
             end
         end
         
-        %   step7   ¨q¥X¥­¿Åµ²ªG
-        fprintf('\t¥­¿Å«á¦U¤¸¯À¤§²öº¸¼Æ\n')
+        % step7   print the result
+        fprintf('\tmole of each element after balancing\n')
         fprintf('\tC         O         H        N\n')
         disp(element);
-        fprintf('\t¥­¿Å«á¦Uª«½è¤§²öº¸¼Æ\n')
+        fprintf('\tmole of each substance after balancing\n')
         fprintf('\tO2        CO        CO2       H2O       H         H2        OH        N2        NO\n')
         n1 = exp(lnn);
         N = exp(lnN);
         disp(n1);
-        fprintf('\t¥­¿Å«áª«½èÁ`²öº¸¼Æ\n')
+        fprintf('\ttotal mole after balancing\n')
         disp(N);
         break;
     else
         
-        % ­Y»~®t­È¤Ó¤j¡AÄ~Äò­pºâ
+        % if the error is too too large, keep calculating
         lnn = lnn + dlnn;
         lnN = lnN + Q(5);
     end
